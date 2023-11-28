5d02b1cb87c5909a9fdae80d3da69110d14df140
VeronikaMyts <152305850+VeronikaMyts@users.noreply.github.com>
2023-11-28 17:56:07 +0200

    Add files via upload

---
+++ OOPC#/Lapa1 OOP.1/Lapa1 OOP.1/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lapa1_oop2
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            
+            Console.Write("Введiть значення a: ");
+            double a = double.Parse(Console.ReadLine());
+
+            Console.Write("Введiть значення b: ");
+            double b = double.Parse(Console.ReadLine());
+           
+            
+            if (a <= b)
+            {
+                
+                double top = Math.Pow(a + b, 5);
+                double down = Math.Pow(b - a, 3);
+
+                
+                if (down == 0)
+                {
+                    Console.WriteLine("Дiлення на нуль не допускається.");
+                }
+                else
+                {
+                    double result = Math.Sqrt(top / down);
+                    Console.WriteLine("Результат: " + result);
+                }
+            }
+            else
+            {
+                Console.WriteLine("Значення a не може бути бiльшим за b.");
+            }
+        }
+
+    }
+}
