1ca1b382fb50dbb045335a849d6a49dc2e8ed5a5
VeronikaMyts <152305850+VeronikaMyts@users.noreply.github.com>
2023-11-28 18:01:48 +0200

    Add files via upload

---
+++ OOPC#/Lapa2.3.OOP/Lapa2.3.OOP/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Lapa2._3.OOP
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = UTF8Encoding.UTF8;
+            Console.WriteLine("Введіть текстовий рядок:");
+            string input = Console.ReadLine();
+
+            // Перевірка кількості відкритих і закритих круглих та квадратних дужок
+            int openParenthesesCount = input.Count(c => c == '(');
+            int closeParenthesesCount = input.Count(c => c == ')');
+            int openSquareBracketsCount = input.Count(c => c == '[');
+            int closeSquareBracketsCount = input.Count(c => c == ']');
+
+            if (openParenthesesCount == closeParenthesesCount && openSquareBracketsCount == closeSquareBracketsCount)
+            {
+                Console.WriteLine("Кількість відкритих і закритих дужок співпадає.");
+            }
+            else
+            {
+                Console.WriteLine("Кількість відкритих і закритих дужок не співпадає.");
+            }
+
+            // Знаходження найдовшого слова
+            string[] words = input.Split(new[] { ' ', '\t', '\n', '\r', '.', ',', '!', '?', ';', ':', '(', ')', '[', ']' }, StringSplitOptions.RemoveEmptyEntries);
+            string longestWord = words.OrderByDescending(w => w.Length).FirstOrDefault();
+
+            if (longestWord != null)
+            {
+                Console.WriteLine($"Найдовше слово: {longestWord}");
+            }
+            else
+            {
+                Console.WriteLine("Немає слів у введеному рядку.");
+            }
+
+            // Видалення 
+            string pattern = @"^[A-Za-z]+$";
+            string result = string.Join(" ", words.Where(word => !Regex.IsMatch(word, pattern)));
+
+            Console.WriteLine($"Текст без слів, що складаються тільки з латинських літер:\n{result}");
+        }
+    }
+}
+
